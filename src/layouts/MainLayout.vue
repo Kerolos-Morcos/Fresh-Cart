247d8d8013daae4cb2d9d37d424f796be558f5ea
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2026-02-05 20:15:26 +0200

    Setting Up Routing

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Footer from '@/components/Footer.vue';
+import Navbar from '@/components/Navbar.vue';
+
+</script>
+
+<template>
+    <Navbar />
+    <RouterView />
+    <Footer />
+</template>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MainLayout from "@/layouts/MainLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/",
+      component: MainLayout,
+      children: [
+        {
+          path: "",
+          name: "home",
+          component: () => import("../pages/Home.vue"),
+        },
+        {
+          path: "about",
+          name: "about",
+          component: () => import("../pages/About.vue"),
+        },
+        {
+          path: "brands",
+          name: "brands",
+          component: () => import("../pages/Brands.vue"),
+        },
+      ],
     },
+    // For Not Displaying Nav & Footer
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
     },
   ],
-})
+});
 
-export default router
+export default router;
